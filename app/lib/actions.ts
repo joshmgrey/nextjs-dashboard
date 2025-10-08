48336f3cdd604f85a05debf09c08afdc3811452d
joshmgrey <joshgrey361@gmail.com>
2025-10-08 17:51:22 -0400

    Log in functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres  from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -29,7 +31,7 @@ const FormSchema = z.object({
   date: z.string(),
 })
 const CreateInvoice = FormSchema.omit({id: true, date: true });
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -60,7 +62,7 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData){
+export async function updateInvoice(id: string, prevState: State, formData: FormData){
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -93,4 +95,22 @@ export async function deleteInvoice(id: string) {
   throw new Error('Failed to Delete Invoice.');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+export async function authenticate(
+  prevState: string | undefined, 
+  formData: FormData,
+) {
+  try {
+     await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
