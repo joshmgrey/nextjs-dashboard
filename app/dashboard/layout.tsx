5cc44db983a2878326b8d8bc30d3653db235e86e
joshmgrey <joshgrey361@gmail.com>
2025-10-08 09:55:11 -0400

    Fixed layout issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,6 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode}){
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
+            <main className="flex-grow overflow-y-auto p-6">{children}</main>
         </div>
     );
 }
