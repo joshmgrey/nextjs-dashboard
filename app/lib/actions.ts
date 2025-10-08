c3884403f1eab6bdb24484455d2ed8c9eec3a668
joshmgrey <joshgrey361@gmail.com>
2025-10-08 19:33:23 -0400

    Add titles

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -92,7 +92,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
   redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice.');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
