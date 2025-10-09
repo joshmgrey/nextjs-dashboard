18fe78ff447ecb76a46bf615e5ebe547f00f8dad
joshmgrey <joshgrey361@gmail.com>
2025-10-09 17:31:25 -0400

    Update Authentication

--- auth.ts
+++ auth.ts
@@ -31,6 +31,8 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) return user;
         }
  
         return null;
